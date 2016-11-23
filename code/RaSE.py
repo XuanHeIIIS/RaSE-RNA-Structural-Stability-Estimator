06cacdf25e224b6e09da82747ba78419c3ee53d5
Fabrizio Costa <xfcosta@gmail.com>
2016-11-23 10:07:41 +0100

    fix help

--- code/RaSE.py
+++ code/RaSE.py
@@ -10,9 +10,11 @@ Author: Fabrizio Costa [costa@informatik.uni-freiburg.de]
 
 Usage:
   RaSE [-i <sequence>]
-      [-k N] [-c N] [-n N] [-w N] [-b N] [-p N] [-r N] [-e N]
-      [-l] [-t] [--draw]
-      [--verbose]
+       [-k N] [-c N, --complexity=N] [-n N, --nbits=N] [-w N, --window_size=N]
+       [-b N] [-p N]
+       [-r N] [-e N]
+       [-l] [-t] [--draw]
+       [--verbose]
   RaSE (-h | --help)
   RaSE --version
 
@@ -20,10 +22,10 @@ Options:
   -i <sequence>                     Specify input sequence [default: stdin].
   -k N                              Specify number of maximally unstable
                                     nucleotides to mark [default: 5].
-  -c N                              Complexity of features [default: 3].
-  -n N                              Size feature pseudo identifiers
-                                    [default: 15].
-  -w N                              Window size [default: 150]
+  -c N, --complexity=N              Complexity of features [default: 3].
+  -n N, --nbits=N                   Num bits to represent all possible feature
+                                    pseudo identifiers [default: 15].
+  -w N, --window_size=N             Window size [default: 150]
   -b N                              Max number of spanning bases [default: 130]
   -p N                              Average probability cutoff [default: 0.1]
   -r N                              Hard threshold [default: 0.5]
@@ -58,6 +60,32 @@ def make_fold(window_size=150,
               max_num_edges=2,
               no_lonely_bps=True,
               nesting=True):
+    """Curry parameters in the folding algorithm.
+
+     Parameters
+    ----------
+    max_num_edges : int (default 2)
+        Is the maximal number of base pair bonds allowed per nucleotide.
+
+    window_size : int (default 150)
+        Is the size of the window.
+
+    max_bp_span : int (default 130)
+        Is the maximum number of bases between to bases that pair.
+
+    avg_bp_prob_cutoff : float (default 0.1)
+        Is the threshold value under which the edge is not materialized.
+
+    no_lonely_bps : bool (default True)
+        If True no lonely base pairs are allowed.
+
+    nesting : bool (default True)
+        If True the edge type is 'nesting'.
+
+    hard_threshold : float (default 0.5)
+        If the edges with avg probability is greater than hard_threshold
+        then they are not of nesting type.
+    """
     fold = curry(rnaplfold_to_eden)(window_size=window_size,
                                     max_bp_span=max_bp_span,
                                     avg_bp_prob_cutoff=avg_bp_prob_cutoff,
@@ -78,32 +106,32 @@ def make_fold_vectorize(complexity=3,
     return fold_vectorize
 
 
-def make_string(gen):
+def _make_string(gen):
     return ''.join(list(gen))
 
 
-def make_variations(seq, index=0, alphabet='ACGU'):
+def _make_variations(seq, index=0, alphabet='ACGU'):
     for nt in alphabet:
         snip = list(seq)
         if snip[index] != nt:
             snip[index] = nt
-            snip = make_string(snip)
+            snip = _make_string(snip)
             yield nt, snip
 
 
 def compute_stability(seq, alphabet='ACGU', fold_vectorize=None):
     # TODO: parallelize indices
     for index in range(len(seq)):
-        cmake_variations = curry(make_variations)(index=index,
-                                                  alphabet=alphabet)
-        make_vecs = compose(fold_vectorize, cmake_variations)
+        _cmake_variations = curry(_make_variations)(index=index,
+                                                    alphabet=alphabet)
+        make_vecs = compose(fold_vectorize, _cmake_variations)
         variation_vecs = make_vecs(seq)
 
         vec = fold_vectorize([('', seq)])
 
         sims = pairwise_kernels(vec, variation_vecs, metric='cosine')
         sims = sims.reshape((len(alphabet) - 1, 1))
-        sims_variations = zip(sims, cmake_variations(seq))
+        sims_variations = zip(sims, _cmake_variations(seq))
 
         score, snip = min([(sim[0], variation[0])
                            for sim, variation in sims_variations])
@@ -117,7 +145,7 @@ def stability(seq, alphabet='ACGU', fold_vectorize=None):
                           alphabet=alphabet,
                           fold_vectorize=fold_vectorize))
     scores = list(scores)
-    snips = make_string(snips)
+    snips = _make_string(snips)
     return snips, scores
 
 
@@ -148,12 +176,26 @@ def rna_structural_stability_estimate(seq,
     return serialize(seq, snips, scores, k=k)
 
 
-def update_graph(graph, scores, snips):
+def _update_graph(graph, scores, snips):
     for u in graph.nodes():
         pos = graph.node[u]['position']
         graph.node[u]['stability'] = scores[pos]
         graph.node[u]['snip'] = snips[pos]
     return graph
+
+
+def _draw(seq, file_name=None, snips=None, scores=None, fold=None):
+    seqs = [('', seq)]
+    graphs = fold(seqs)
+    graph = graphs.next()
+    graph = _update_graph(graph, scores, snips)
+
+    opts = {'size': 14, 'vertex_border': False, 'vertex_size': 400,
+            'edge_label': None, 'font_size': 9, 'vertex_alpha': 0.6,
+            'invert_colormap': True, 'secondary_vertex_label': 'snip',
+            'vertex_color': 'stability', 'colormap': 'Blues',
+            'ignore_for_layout': 'nesting', 'layout': 'KK'}
+    draw_graph(graph, file_name=file_name, **opts)
 
 
 def draw(seq,
@@ -165,28 +207,18 @@ def draw(seq,
                                          nbits=nbits,
                                          fold=fold)
     snips, scores = stability(seq, fold_vectorize=fold_vectorize)
-
-    seqs = [('', seq)]
-    graphs = fold(seqs)
-    graph = graphs.next()
-    graph = update_graph(graph, scores, snips)
-
-    opts = {'size': 14, 'vertex_border': False, 'vertex_size': 400,
-            'edge_label': None, 'font_size': 9, 'vertex_alpha': 0.6,
-            'invert_colormap': True, 'secondary_vertex_label': 'snip',
-            'vertex_color': 'stability', 'colormap': 'Blues',
-            'ignore_for_layout': 'nesting', 'layout': 'KK'}
-    draw_graph(graph, file_name=file_name, **opts)
+    _draw(seq, file_name=file_name, snips=snips, scores=scores, fold=fold)
 
 
 def main(args):
     """Main."""
-    # setup variables
+    # read variables
+    # if no -i is given then read from stdin
     seq = (sys.stdin.readline().strip() if args['-i'] == 'stdin' else args['-i'])
     k = int(args['-k'])
-    complexity = int(args['-c'])
-    nbits = int(args['-n'])
-    window_size = int(args['-w'])
+    complexity = int(args['--complexity'][0])
+    nbits = int(args['--nbits'][0])
+    window_size = int(args['--window_size'][0])
     window_size = min(len(seq), window_size)
     max_bp_span = int(args['-b'])
     max_bp_span = min(len(seq), max_bp_span)
@@ -199,7 +231,7 @@ def main(args):
         nesting = False
     else:
         nesting = True
-    # logger
+    # setup logger
     if args['--verbose']:
         verbosity = 2
     else:
@@ -209,6 +241,7 @@ def main(args):
                       filename='log')
     logger.debug(serialize_dict(args))
 
+    # setup folding algorithm
     fold = make_fold(window_size=window_size,
                      max_bp_span=max_bp_span,
                      avg_bp_prob_cutoff=avg_bp_prob_cutoff,
@@ -216,19 +249,24 @@ def main(args):
                      max_num_edges=max_num_edges,
                      no_lonely_bps=no_lonely_bps,
                      nesting=nesting)
-    rase = curry(rna_structural_stability_estimate)(k=k,
-                                                    complexity=complexity,
-                                                    nbits=nbits,
-                                                    fold=fold)
-    for line in rase(seq):
+    # setup vectorizer
+    fold_vectorize = make_fold_vectorize(complexity=complexity,
+                                         nbits=nbits,
+                                         fold=fold)
+    # compute the most de-stabilizing single nt change per position
+    # and the corresponding similarity of the resulting structure
+    # w.r.t. the original
+    snips, scores = stability(seq,
+                              alphabet='ACGU',
+                              fold_vectorize=fold_vectorize)
+    # print: nt pos, original nt, most de-stabilizing nt, similarity score
+    for line in serialize(seq, snips, scores, k=k):
         print(line)
 
+    # if drawing is required use the folding algorithm to compute the graph
     if args['--draw']:
-        draw(seq,
-             fold=fold,
-             complexity=complexity,
-             nbits=nbits,
-             file_name='out.pdf')
+        _draw(seq, file_name='out.pdf', snips=snips, scores=scores, fold=fold)
+
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='RaSE v1.0')
